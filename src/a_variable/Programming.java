22532a8113dd499418bdfd557e7d46c19ff6aa47
junyoung <junyeong.b2n2w@gmail.com>
2020-06-24 16:14:14 +0900

    first commit

---
+++ src/a_variable/Programming.java
@@ -0,0 +1,20 @@
+package a_variable;
+
+public class Programming {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 
+		 * 
+		 * 앞에서부터 하나씩 비교해서 큰값을 기억한다
+		 * 
+		 */
+		
+		for(int i =0; i<10; i++){
+			System.out.print((int)(Math.random() * 100) + " ");
+			
+		}
+	}
+
+}
---
+++ src/a_variable/Variable.java
@@ -0,0 +1,72 @@
+package a_variable;
+
+public class Variable {
+	
+	public static void main(String[] args) {
+		
+		int name;
+//		double name2;
+//		
+	name = 10;
+//		name2 = 2.5;
+//		
+//		
+//		int abc = 30;
+//		long l = 40L;
+//		float f = 5.5f;
+//		char c = '한';
+//		boolean b = true;
+//		
+//		
+//	
+		byte v1 = 1;
+		short v2 = 2;
+		int v3 = 3;
+		long v4 = 4L;
+		
+		float v5 = 1.4f;
+		double v6 = 2.2;
+		
+		char v7 = 'a';
+		
+		boolean v8 = false;
+//		
+//		System.out.println(name);
+//		
+//		name = 100;
+//		
+//		System.out.println(name);
+//		
+		
+		v1 = 100;
+		v2 = 200;
+		v3 = 300;
+		v4 = 400L;
+		v5 = 3.14f;
+		v6 = 3.14;
+		v7 = 'A';
+		v8 = true;
+		
+		System.out.println(v1);
+		System.out.println(v2);
+		System.out.println(v3);
+		System.out.println(v4);
+		System.out.println(v5);
+		System.out.println(v6);
+		System.out.println(v7);
+		System.out.println(v8);
+		
+		String str = "123213";
+		System.out.println(str);
+		
+		final int MAX_NUMBER; 
+		MAX_NUMBER = 100;
+	
+		System.out.println(MAX_NUMBER);
+		
+		System.out.print("줄바꿈을 하지 않는다.");
+		System.out.print("줄바꿈을 하지 않는다.\n");
+		System.out.println("탭을\t한다." + name);
+		System.out.printf("문자열 : %s, 숫자 : %d", "Hello", 10);
+	}
+}
